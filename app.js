5b29ee2f273eaa8c9a44c81905f275950d13d07e
hieu90 <doanconghieuksdt@gmail.com>
2021-06-11 14:00:32 +0700

    update at 2021-05-10 : 14h00

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-var express = require('express');
-var appp = express();
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/server', (req,res)=>{
+    main = 'hello';
+    res.render('index', {main});
+})
+
+app.listen(3000, ()=>{
+    console.log('Server Opened!');
+});
